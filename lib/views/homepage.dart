ce2d261348c7b6c697ceff808982dec6547ea1fc
shehap62 <shehapkhalil62@gmail.com>
2024-03-30 17:49:46 +0200

    edit

--- lib/views/homepage.dart
+++ lib/views/homepage.dart
@@ -23,7 +23,7 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    Timer.periodic(const Duration(seconds: 59), (timer) {
+    Timer.periodic(const Duration(seconds: 1), (timer) {
       controller.updateTime();
     });
     return Obx(
